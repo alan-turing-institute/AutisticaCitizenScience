0a602b02c1565a1aa9fabfefcd11dd2ae41e4920
Thomas Stanley <thomas.stanley@uk.fujitsu.com>
2019-09-26 10:39:37 +0100

    adjusted app.js for express server

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const api = require('./server/routes/api')
+//const api = require('./server/routes/api')
 
 const app = express();
 
-app.use(express.static(path.join(__dirname,'Experiences/dist')));
+app.use(express.static(path.join(__dirname,'Experiences/dist/Experiences')));
 app.get('*',(req, res) => {
-  res.sendFile(path.join(__dirname,'Experiences/dist/index.html'));
+  res.sendFile(path.join(__dirname,'Experiences/dist/Experiences/index.html'));
   });
   
 const port = process.env.PORT || '3001';
